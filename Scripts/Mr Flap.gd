dac89246bc6beca039c06a5dc30ecb9897e2969d
Lorenzo De Liguori <85902885+Chenzo46@users.noreply.github.com>
2023-09-21 21:14:34 -0400

    Added PS

--- Scripts/Mr Flap.gd
+++ Scripts/Mr Flap.gd
@@ -1,9 +1,10 @@
 extends RigidBody2D
 @export var jump_strength:float = 600
-
 func _ready():
 	$Sprite2D.modulate = Color(30,1,0)
-
+	var particle_material:ParticleProcessMaterial = $Particles.process_material
+	var grad:GradientTexture1D = particle_material.color_ramp
+	print(grad.has_alpha())
 func _process(delta):
 	#Jump Code
 	if Input.is_action_just_pressed("Jump"):
@@ -14,4 +15,4 @@ func _on_body_entered(body):
 		end_player()
 
 func end_player():
-	GameStateManager._restart_game()
+	GameStateManager._restart_game() 
