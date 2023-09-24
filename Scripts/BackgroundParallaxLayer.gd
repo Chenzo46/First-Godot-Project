38ca14ca098d8345bcd6610eb374f3828e40d525
Chenzo46 <85902885+Chenzo46@users.noreply.github.com>
2023-09-24 18:35:39 -0400

    Update -LPA

--- Scripts/BackgroundParallaxLayer.gd
+++ Scripts/BackgroundParallaxLayer.gd
@@ -2,8 +2,8 @@ class_name BackgroundParallaxLayer extends Node2D
 	
 @export var parts:Array[Node2D] = []
 @export var scroll_speed:float = 200
-@export var min:float
-@export var max:float
+@export var min_rng:float
+@export var max_rng:float
 	
 func _set_parts(p1:Node2D, p2:Node2D, p3:Node2D):
 	parts = [p1,p2,p3]
@@ -14,5 +14,5 @@ func _set_speed(new_speed:float):
 func _process(delta):
 	for c in parts:
 		c.position.x -= delta * scroll_speed
-		if c.position.x < min:
-			c.position.x = max
+		if c.position.x < min_rng:
+			c.position.x = max_rng
